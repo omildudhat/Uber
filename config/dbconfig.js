b36f799f851cfe47f124116aab7c844e42fb7281
Ynova019 <ynova019@gmail.com>
2022-09-26 23:53:03 -0500

    initial commit

---
+++ config/dbconfig.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const mysql = require('mysql');
+
+var connection;
+
+connection = mysql.createConnection({
+    host     : process.env.HOST,
+    user     : process.env.USER,
+    password : process.env.PASSWORD,
+    port     : process.env.PORT,
+    database : process.env.DATABASE
+  });
+  
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+  
+    console.log('Connected to database.');
+  });
+
+
+  module.exports = connection;
